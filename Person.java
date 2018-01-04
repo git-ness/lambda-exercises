9e7e2caca9415df3086cb5c1d0fcabb6477b893e
Elsa Hill <ehill@newrelic.com>
2018-01-04 13:18:26 -0800

    First go

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int i) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = i;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
+
+}
---
+++ Unit1Exercise.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise {
+
+    public static void main(String[] args) {
+
+        // https://javabrains.io/courses/java_lambdabasics/lessons/Lambda-Exercise/
+
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thoams", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45)
+
+
+                // ----- Using a Lambda expression: ------
+
+                // Step 1: Sort list by last name
+                // Step 2: Create a method that prints all elements in the list
+                // Step 3: Create a method that prints all people that have last name beginning with C
+
+        );
+
+        people.sort((Person person1, Person person2) -> person1.getLastname().compareTo(person2.getLastname()));
+
+        for (Person peeps : people) {
+            System.out.println(peeps.getLastname());
+        }
+
+    }
+}
